5e8112658958afbb9c83c96add8270db03e88748
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-06-25 17:52:11 +0300

    feat: add multi container sortable

--- src/entities/board/ui/board.tsx
+++ src/entities/board/ui/board.tsx
@@ -1,33 +1,114 @@
-import { useDroppable } from '@dnd-kit/core'
-import { SortableContext } from '@dnd-kit/sortable'
+import { SortableContext, useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 import { SortableItem } from '@shared/ui/sortable-item'
 
 interface Props {
-  id: string
-  items: string[]
+  board: Column
+  items: Task[]
 }
 
-export const Board = ({ id, items }: Props) => {
-  const { setNodeRef } = useDroppable({
-    id,
+interface Column {
+  id: string
+  title: string
+}
+
+interface Task {
+  id: string
+  columnId: string
+  content: string
+}
+
+export const Board = ({ board, items }: Props) => {
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: board.id,
+    data: {
+      type: 'Column',
+      board,
+    },
   })
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  }
+
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="flex h-[500px] max-h-[500px] w-[350px] flex-col rounded-md border-2 border-pink-500 bg-black opacity-40"
+      />
+    )
+  }
 
   return (
     <SortableContext items={items}>
-      <div className="rounded-md bg-white p-5">
+      <div ref={setNodeRef} style={style} className="rounded-md bg-white p-5">
         <div className="mb-2 border-b-2 border-black py-2 font-medium">
           To do 2
         </div>
-        <ul ref={setNodeRef} className="flex flex-col gap-2">
+        <ul className="flex flex-col gap-2">
           {items.map((item) => (
-            <SortableItem key={item} id={item}>
-              <li className="rounded-md border-2 border-b-4 border-black bg-white p-2">
-                выучить effector
-              </li>
+            <SortableItem key={item.id} id={item.id}>
+              <TaskCard task={item} />
             </SortableItem>
           ))}
         </ul>
       </div>
     </SortableContext>
+  )
+}
+
+export const TaskCard = ({ task }: Task) => {
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: task.id,
+    data: {
+      type: 'Task',
+      task,
+    },
+  })
+
+  console.log(task)
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  }
+
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="relative flex h-[100px] min-h-[100px] cursor-grab items-center rounded-xl border-2 border-black bg-gray-200 p-2.5 text-left opacity-30"
+      />
+    )
+  }
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className="rounded-md border-2 border-b-4 border-black bg-white p-2"
+    >
+      {task.content}
+    </div>
   )
 }
--- src/widgets/kanban/ui/kanban.tsx
+++ src/widgets/kanban/ui/kanban.tsx
@@ -2,131 +2,226 @@ import {
   DndContext,
   DragEndEvent,
   DragOverEvent,
-  DragOverlay,
   DragStartEvent,
+  PointerSensor,
+  useSensor,
+  useSensors,
 } from '@dnd-kit/core'
-import { arrayMove } from '@dnd-kit/sortable'
+import { arrayMove, SortableContext } from '@dnd-kit/sortable'
 import { Board } from '@entities/board'
-import { Item } from '@shared/ui/item'
 import { useState } from 'react'
 
+interface Column {
+  id: string
+  title: string
+}
+
+interface Task {
+  id: string
+  columnId: string
+  content: string
+}
+
+const defaultCols: Column[] = [
+  {
+    id: 'todo',
+    title: 'Todo',
+  },
+  {
+    id: 'doing',
+    title: 'Work in progress',
+  },
+  {
+    id: 'done',
+    title: 'Done',
+  },
+]
+
+const defaultTasks: Task[] = [
+  {
+    id: '1',
+    columnId: 'todo',
+    content: 'List admin APIs for dashboard',
+  },
+  {
+    id: '2',
+    columnId: 'todo',
+    content:
+      'Develop user registration functionality with OTP delivered on SMS after email confirmation and phone number confirmation',
+  },
+  {
+    id: '3',
+    columnId: 'doing',
+    content: 'Conduct security testing',
+  },
+  {
+    id: '4',
+    columnId: 'doing',
+    content: 'Analyze competitors',
+  },
+  {
+    id: '5',
+    columnId: 'done',
+    content: 'Create UI kit documentation',
+  },
+  {
+    id: '6',
+    columnId: 'done',
+    content: 'Dev meeting',
+  },
+  {
+    id: '7',
+    columnId: 'done',
+    content: 'Deliver dashboard prototype',
+  },
+  {
+    id: '8',
+    columnId: 'todo',
+    content: 'Optimize application performance',
+  },
+  {
+    id: '9',
+    columnId: 'todo',
+    content: 'Implement data validation',
+  },
+  {
+    id: '10',
+    columnId: 'todo',
+    content: 'Design database schema',
+  },
+  {
+    id: '11',
+    columnId: 'todo',
+    content: 'Integrate SSL web certificates into workflow',
+  },
+  {
+    id: '12',
+    columnId: 'doing',
+    content: 'Implement error logging and monitoring',
+  },
+  {
+    id: '13',
+    columnId: 'doing',
+    content: 'Design and implement responsive UI',
+  },
+]
+
 export const Kanban = () => {
-  const [activeId, setActiveId] = useState(null)
-  const [items, setItems] = useState({
-    'to-do': ['1', '2', '3'],
-    'in-progress': ['4', '5', '6'],
-  })
+  const [columns, setColumns] = useState(defaultCols)
+  const columnsId = columns.map((col) => col.id)
 
-  const findContainer = (id) => {
-    if (id in items) {
-      return id
-    }
+  const [tasks, setTasks] = useState(defaultTasks)
 
-    return Object.keys(items).find((key) => items[key].includes(id))
-  }
+  const [activeColumn, setActiveColumn] = useState<Column | null>(null)
 
-  const handleDragStart = (event: DragStartEvent) => {
-    const { active } = event
+  const [activeTask, setActiveTask] = useState<Task | null>(null)
 
-    setActiveId(active.id)
-  }
-
-  const handleDragOver = (event: DragOverEvent) => {
-    const { active, over, draggingRect } = event
-    const { id } = active
-    const { id: overId } = over
-
-    // Find the containers
-    const activeContainer = findContainer(id)
-    const overContainer = findContainer(overId)
-
-    if (
-      !activeContainer ||
-      !overContainer ||
-      activeContainer === overContainer
-    ) {
-      return
-    }
-
-    setItems((prev) => {
-      const activeItems = prev[activeContainer]
-      const overItems = prev[overContainer]
-
-      // Find the indexes for the items
-      const activeIndex = activeItems.indexOf(id)
-      const overIndex = overItems.indexOf(overId)
-
-      let newIndex
-      if (overId in prev) {
-        // We're at the root droppable of a container
-        newIndex = overItems.length + 1
-      } else {
-        const isBelowLastItem =
-          over &&
-          overIndex === overItems.length - 1 &&
-          draggingRect.offsetTop > over.rect.offsetTop + over.rect.height
-
-        const modifier = isBelowLastItem ? 1 : 0
-
-        newIndex = overIndex >= 0 ? overIndex + modifier : overItems.length + 1
-      }
-
-      return {
-        ...prev,
-        [activeContainer]: [
-          ...prev[activeContainer].filter((item) => item !== active.id),
-        ],
-        [overContainer]: [
-          ...prev[overContainer].slice(0, newIndex),
-          items[activeContainer][activeIndex],
-          ...prev[overContainer].slice(newIndex, prev[overContainer].length),
-        ],
-      }
-    })
-  }
-
-  const handleDragEnd = (event: DragEndEvent) => {
-    const { active, over } = event
-
-    const activeContainer = findContainer(active.id)
-    const overContainer = findContainer(over.id)
-
-    if (
-      !activeContainer ||
-      !overContainer ||
-      activeContainer !== overContainer
-    ) {
-      return
-    }
-
-    const activeIndex = items[activeContainer].indexOf(active.id)
-    const overIndex = items[overContainer].indexOf(over.id)
-
-    if (activeIndex !== overIndex) {
-      setItems((items) => ({
-        ...items,
-        [overContainer]: arrayMove(
-          items[overContainer],
-          activeIndex,
-          overIndex,
-        ),
-      }))
-    }
-
-    setActiveId(null)
-  }
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 10,
+      },
+    }),
+  )
 
   return (
     <DndContext
-      onDragStart={handleDragStart}
-      onDragOver={handleDragOver}
-      onDragEnd={handleDragEnd}
+      sensors={sensors}
+      onDragStart={onDragStart}
+      onDragEnd={onDragEnd}
+      onDragOver={onDragOver}
     >
-      <ul className="grid grid-cols-3 gap-2">
-        <Board id="todo" items={items['to-do']} />
-        <Board id="inprogress" items={items['in-progress']} />
-        <DragOverlay>{activeId ? <Item>Hello</Item> : null}</DragOverlay>
-      </ul>
+      <SortableContext items={columnsId}>
+        <ul className="grid grid-cols-3 gap-2">
+          {columns.map((column) => (
+            <Board
+              key={column.id}
+              board={column}
+              items={tasks.filter((task) => task.columnId === column.id)}
+            />
+          ))}
+        </ul>
+      </SortableContext>
     </DndContext>
   )
+
+  function onDragStart(event: DragStartEvent) {
+    if (event.active.data.current?.type === 'Column') {
+      setActiveColumn(event.active.data.current.column)
+    }
+
+    if (event.active.data.current?.type === 'Task') {
+      setActiveTask(event.active.data.current.task)
+    }
+  }
+
+  function onDragEnd(event: DragEndEvent) {
+    setActiveColumn(null)
+    setActiveTask(null)
+
+    const { active, over } = event
+    if (!over) return
+
+    const activeId = active.id
+    const overId = over.id
+
+    if (activeId === overId) return
+
+    const isActiveAColumn = active.data.current?.type === 'Column'
+    if (!isActiveAColumn) return
+
+    console.log('DRAG END')
+
+    setColumns((columns) => {
+      const activeColumnIndex = columns.findIndex((col) => col.id === activeId)
+
+      const overColumnIndex = columns.findIndex((col) => col.id === overId)
+
+      return arrayMove(columns, activeColumnIndex, overColumnIndex)
+    })
+  }
+
+  function onDragOver(event: DragOverEvent) {
+    const { active, over } = event
+    if (!over) return
+
+    const activeId = active.id
+    const overId = over.id
+
+    if (activeId === overId) return
+
+    const isActiveATask = active.data.current?.type === 'Task'
+    const isOverATask = over.data.current?.type === 'Task'
+
+    if (!isActiveATask) return
+
+    // Im dropping a Task over another Task
+    if (isActiveATask && isOverATask) {
+      setTasks((tasks) => {
+        const activeIndex = tasks.findIndex((t) => t.id === activeId)
+        const overIndex = tasks.findIndex((t) => t.id === overId)
+
+        if (tasks[activeIndex].columnId !== tasks[overIndex].columnId) {
+          // Fix introduced after video recording
+          tasks[activeIndex].columnId = tasks[overIndex].columnId
+          return arrayMove(tasks, activeIndex, overIndex - 1)
+        }
+
+        return arrayMove(tasks, activeIndex, overIndex)
+      })
+    }
+
+    const isOverAColumn = over.data.current?.type === 'Column'
+
+    // Im dropping a Task over a column
+    if (isActiveATask && isOverAColumn) {
+      setTasks((tasks) => {
+        const activeIndex = tasks.findIndex((t) => t.id === activeId)
+
+        tasks[activeIndex].columnId = overId
+        console.log('DROPPING TASK OVER COLUMN', { activeIndex })
+        return arrayMove(tasks, activeIndex, activeIndex)
+      })
+    }
+  }
 }
