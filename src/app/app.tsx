c6c77d6601d753f0925f2c37aa5d39a11ad36b55
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-06-25 20:24:36 +0300

    feat: add layout

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,10 @@
+import { LayoutBase } from '@layouts/base'
 import { Kanban } from '@widgets/kanban'
 
 export const App = () => {
   return (
-    <div className="h-dvh w-dvw">
-      <div className="container mx-auto px-2.5">
-        <header className="py-10">
-          <h1 className="text-center text-4xl font-bold uppercase">Kanban</h1>
-        </header>
-        <Kanban />
-      </div>
-    </div>
+    <LayoutBase>
+      <Kanban />
+    </LayoutBase>
   )
 }
---
+++ src/layouts/base/ui/base.tsx
@@ -0,0 +1,62 @@
+import { ReactNode } from 'react'
+
+interface Props {
+  children: ReactNode
+}
+
+export const LayoutBase = ({ children }: Props) => {
+  return (
+    <div className="flex h-dvh w-dvw flex-col">
+      <header className="mb-2 border-b border-gray-300 bg-white py-5">
+        <div className="container mx-auto px-2.5">
+          <div className="flex items-center gap-2">
+            <div className="h-10 w-10">
+              <img src="/icons/logo.svg" alt="logo" />
+            </div>
+            <h1 className="text-4xl font-bold text-gray-600">Kanban</h1>
+          </div>
+        </div>
+      </header>
+      <main className="container mx-auto flex-1 px-2.5">{children}</main>
+      <footer className="border-t border-gray-300 bg-white py-5">
+        <div className="container mx-auto flex items-center justify-between px-2.5">
+          <div className="flex items-center gap-2">
+            <div className="h-5 w-5">
+              <img src="/icons/logo.svg" alt="logo" />
+            </div>
+            <h2 className="text-xl font-semibold text-gray-600">Kanban</h2>
+          </div>
+          <ul className="flex items-center gap-2">
+            <li>
+              <a href="#" className="font-medium text-gray-600">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="font-medium text-gray-600">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="font-medium text-gray-600">
+                Cookie Policy
+              </a>
+            </li>
+          </ul>
+          <ul className="flex items-center gap-2">
+            <li className="h-8 w-8">
+              <a href="https://github.com/DenisEgorov200">
+                <img src="/icons/github.svg" alt="github" />
+              </a>
+            </li>
+            <li className="h-8 w-8">
+              <a href="https://t.me/feaxh">
+                <img src="/icons/telegram.svg" alt="telegram" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+  )
+}
