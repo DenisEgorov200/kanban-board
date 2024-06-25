9bb58062a2f8c603ba808c9a2152e9dbe9ff49e5
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-06-25 12:07:26 +0300

    fix: fix <tsconfigRootDir>/.eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,40 +4,34 @@ const {
 } = require("eslint-kit");
 
 module.exports = configure({
-    allowDebug: process.env.NODE_ENV !== "production",
+  allowDebug: process.env.NODE_ENV !== "production",
 
-    include: ["vite.config.ts", ".eslintrc.cjs"],
+  presets: [
+    presets.imports(),
+    presets.node(),
+    presets.prettier(),
+    presets.typescript(),
+    presets.react()
+  ],
 
-    parserOptions: {
-      project:  ['./tsconfig.json', './tsconfig.node.json'],
-      tsconfigRootDir: __dirname,
-    },
-
-    presets: [
-        presets.imports(),
-        presets.node(),
-        presets.prettier(),
-        presets.typescript(),
-        presets.react()
-    ],
-
-    rules: {
-      "prettier/prettier": [
-        "error",
-        {
-          "endOfLine": "auto"
-        }
-      ]
-    },
-      
-    settings: {
-      'import/resolver': {
-        alias: {
-          extensions: [".js", ".jsx", ".ts", ".tsx"],
-          map: [
-            ['@', './src'],
-          ],
-        },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "auto"
+      }
+    ]
+  },
+  
+  settings: {
+    'import/resolver': {
+      alias: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        map: [
+          ['@', './src'],
+        ],
       },
     },
+  },
+
 });
