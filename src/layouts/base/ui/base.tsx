3e1182f1d2fa5b1bc767fe1a8b02268f6f5acc6b
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-06-25 20:50:34 +0300

    feat(header): add user info

--- src/layouts/base/ui/base.tsx
+++ src/layouts/base/ui/base.tsx
@@ -8,12 +8,30 @@ export const LayoutBase = ({ children }: Props) => {
   return (
     <div className="flex h-dvh w-dvw flex-col">
       <header className="mb-2 border-b border-gray-300 bg-white py-5">
-        <div className="container mx-auto px-2.5">
+        <div className="container mx-auto flex items-center justify-between px-2.5">
           <div className="flex items-center gap-2">
             <div className="h-10 w-10">
               <img src="/icons/logo.svg" alt="logo" />
             </div>
             <h1 className="text-4xl font-bold text-gray-600">Kanban</h1>
+          </div>
+          <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2">
+              <p className="font-medium text-gray-600">user-name</p>
+              <div className="h-8 w-8 overflow-hidden rounded-full">
+                <img src="https://placehold.co/100x100" alt="" />
+              </div>
+            </div>
+            <div className="flex items-center">
+              <img
+                src="/icons/notification.svg"
+                alt="notification"
+                className="h-8 w-8"
+              />
+            </div>
+            <div className="flex items-center">
+              <img src="/icons/logout.svg" alt="logout" className="h-6 w-6" />
+            </div>
           </div>
         </div>
       </header>
