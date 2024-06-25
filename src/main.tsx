0230f31351aa804e852304dffe44733ea1e8e0a2
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-06-25 23:19:34 +0300

    feat: add app-started

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import './app/styles/index.css'
+import { appStarted } from '@shared/init/index.ts'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app/app.tsx'
+
+appStarted()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
