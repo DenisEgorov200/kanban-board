fc567e862e94c9a55f079a7ebd09458fe2cd7082
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-06-25 23:21:41 +0300

    feat: add routing

---
+++ src/shared/routing/routes.ts
@@ -0,0 +1,18 @@
+import { createRoute, UnmappedRouteObject } from 'atomic-router'
+
+export const routes = {
+  home: createRoute(),
+  auth: {
+    signIn: createRoute(),
+    finish: createRoute(),
+  },
+}
+
+export const pageNotFoundRoute = createRoute()
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const routesMap: UnmappedRouteObject<any>[] = [
+  { path: '/', route: routes.home },
+  { path: '/auth/sign-in', route: routes.auth.signIn },
+  { path: '/auth/finish', route: routes.auth.finish },
+]
