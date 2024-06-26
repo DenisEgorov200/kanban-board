f65e865597e68d909c97983cceb5cb934743f258
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-06-26 14:19:41 +0300

    feat: add auth finish, try again click

--- src/pages/auth/finish/ui/finish.tsx
+++ src/pages/auth/finish/ui/finish.tsx
@@ -1,14 +1,21 @@
+import { useUnit } from 'effector-react'
+import { $pending, $successfully, tryAgainClicked } from '../model'
+
 export const AuthFinishPage = () => {
+  const [pending, successfully] = useUnit([$pending, $successfully])
+
   return (
     <div className="flex h-dvh w-dvw flex-col items-center justify-center">
       <div className="mb-12 flex items-center gap-2">
         <h1 className="text-6xl font-extrabold text-blue-600">Kanban</h1>
       </div>
-      {/* <LoginValidating /> */}
-
-      {/* <LoginFinished /> */}
-
-      <LoginFailed />
+      {pending ? (
+        <LoginValidating />
+      ) : successfully ? (
+        <LoginFinished />
+      ) : (
+        <LoginFailed />
+      )}
     </div>
   )
 }
@@ -37,6 +44,8 @@ const LoginFinished = () => {
 }
 
 const LoginFailed = () => {
+  const handleTryAgain = useUnit(tryAgainClicked)
+
   return (
     <>
       <h2 className="mb-2 text-2xl font-bold text-red-400">Sign In Failed</h2>
@@ -44,7 +53,10 @@ const LoginFailed = () => {
         We encountered an issue validating your sign-in link. Please ensure the
         link hasn't expired or been used before.
       </p>
-      <button className="flex items-center justify-center gap-2 rounded-md border border-gray-300 bg-white px-5 py-2.5 transition-colors hover:bg-gray-100">
+      <button
+        onClick={() => handleTryAgain()}
+        className="flex items-center justify-center gap-2 rounded-md border border-gray-300 bg-white px-5 py-2.5 transition-colors hover:bg-gray-100"
+      >
         Try again
       </button>
     </>
