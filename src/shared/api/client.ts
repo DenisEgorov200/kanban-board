91cc35932cdd934e7cbfb05a35c77057c8eb24b8
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-06-26 12:21:08 +0300

    feat: add database types

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -1,4 +1,5 @@
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from '@shared/config'
 import { createClient } from '@supabase/supabase-js'
+import { Database } from './database.types'
 
-export const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const client = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY)
