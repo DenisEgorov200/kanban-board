a54725a2843da3b4927784516ed4f2c21b836540
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-06-27 17:21:00 +0300

    feat: add todo-card

--- src/pages/tasks/ui/tasks.tsx
+++ src/pages/tasks/ui/tasks.tsx
@@ -1,3 +1,20 @@
+import { TodoCard } from '@entities/todo-card'
+
 export const TasksPage = () => {
-  return <div>my-tasks</div>
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center gap-2">
+        <div>Status</div>
+        <div>Sort by</div>
+      </div>
+      <ul className="flex flex-col gap-2">
+        <li>
+          <TodoCard />
+        </li>
+        <li>
+          <TodoCard />
+        </li>
+      </ul>
+    </div>
+  )
 }
