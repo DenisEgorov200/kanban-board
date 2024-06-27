6eac987b4984795d5acf8adeb66189321f5a22bb
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-06-27 17:40:06 +0300

    style(todo-card): add links, settings, name

--- src/entities/todo-card/ui/todo-card.tsx
+++ src/entities/todo-card/ui/todo-card.tsx
@@ -2,8 +2,24 @@ import { CheckMark } from '@shared/ui/check-mark'
 
 export const TodoCard = () => {
   return (
-    <div>
-      <CheckMark />
+    <div className="flex items-center justify-between gap-2 border-b border-gray-200 py-5">
+      <div className="flex items-center gap-4">
+        <CheckMark />
+        <h2 className="font-medium text-gray-600">
+          Develop user registration functionality with OTP delivered on SMS
+          after email confirmation and phone number confirmation
+        </h2>
+      </div>
+      <div>
+        <ul className="flex items-center gap-4">
+          <li className="h-5 w-5 cursor-pointer">
+            <img src="/icons/hyperlink.svg" alt="hyperlink" />
+          </li>
+          <li className="h-5 w-5 cursor-pointer">
+            <img src="/icons/settings.svg" alt="settings" />
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
