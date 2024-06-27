ddd628afa227a28e7c5c23ec048e8096a1d79e34
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-06-27 18:48:43 +0300

    feat: add select

--- src/pages/tasks/ui/tasks.tsx
+++ src/pages/tasks/ui/tasks.tsx
@@ -1,11 +1,25 @@
 import { TodoCard } from '@entities/todo-card'
+import { Select } from '@shared/ui/select'
+import { useState } from 'react'
 
 export const TasksPage = () => {
+  const [selectStatus, setSelectStatus] = useState('React')
+
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex items-center gap-2">
-        <div>Status</div>
-        <div>Sort by</div>
+      <div className="flex items-center gap-4">
+        <Select
+          label="Status"
+          placeholder="Any"
+          data={['React', 'Angular', 'Vue', 'Svelte', 'Effector']}
+          value={selectStatus}
+          onChange={setSelectStatus}
+        />
+        <Select
+          label="Sort by"
+          placeholder="Sort"
+          data={['React', 'Angular', 'Vue', 'Svelte']}
+        />
       </div>
       <ul className="flex flex-col gap-2">
         <li>
