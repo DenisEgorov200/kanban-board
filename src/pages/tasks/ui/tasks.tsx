f78813f99981113f35ea1ff8a6f92eddc3fffa76
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-07-01 14:49:02 +0300

    feat: add controlled select

--- src/pages/tasks/ui/tasks.tsx
+++ src/pages/tasks/ui/tasks.tsx
@@ -1,12 +1,21 @@
 import { TodoCard } from '@entities/todo-card'
 import { Select } from '@shared/ui/select'
+import { useState } from 'react'
 
 export const TasksPage = () => {
+  const [value, setValue] = useState('')
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex items-center gap-4">
-        <Select />
-        <Select />
+        <Select
+          label="Status"
+          placeholder="Any"
+          data={['apple', 'banana']}
+          value={value}
+          onChange={setValue}
+        />
+        <Select data={['apple', 'banana']} />
       </div>
       <ul className="flex flex-col gap-2">
         <li>
--- src/shared/ui/select/select.tsx
+++ src/shared/ui/select/select.tsx
@@ -1,47 +1,64 @@
 import * as RadixSelect from '@radix-ui/react-select'
 import { clsx } from 'clsx'
-import { forwardRef, ReactNode, RefAttributes } from 'react'
+import { forwardRef, ReactNode } from 'react'
 
-export const Select = () => {
+interface Props {
+  label?: string
+  placeholder?: string
+  data: string[]
+  value?: string
+  onChange?: (value: string) => void
+}
+
+export const Select = ({
+  label,
+  placeholder,
+  data,
+  value,
+  onChange,
+}: Props) => {
   return (
-    <RadixSelect.Root>
-      <RadixSelect.Trigger
-        className="inline-flex min-w-28 items-center justify-between gap-2 rounded border border-transparent bg-blue-50 px-4 py-2.5 font-medium leading-none text-gray-600 outline-none focus:border-gray-200"
-        aria-label="Status"
-      >
-        <RadixSelect.Value
-          placeholder="Any"
-          className="max-w-12 overflow-ellipsis"
-        />
-        <RadixSelect.Icon className="h-5 w-5 -rotate-90">
-          <img src="/icons/arrow.svg" alt="arrow" />
-        </RadixSelect.Icon>
-      </RadixSelect.Trigger>
-      <RadixSelect.Portal>
-        <RadixSelect.Content
-          className="overflow-hidden rounded-md bg-white shadow-xl"
-          align="center"
-          position="popper"
-          sideOffset={10}
+    <div className="flex items-center gap-2">
+      {label ? <p className="font-medium text-gray-600">{label}</p> : null}
+      <RadixSelect.Root value={value} onValueChange={onChange}>
+        <RadixSelect.Trigger
+          className="inline-flex min-w-28 items-center justify-between gap-2 rounded border border-transparent bg-blue-50 px-4 py-2.5 font-medium capitalize leading-none text-gray-600 outline-none focus:border-gray-200"
+          aria-label="Status"
         >
-          <RadixSelect.ScrollUpButton className="flex h-5 w-5 rotate-90 cursor-default items-center justify-center bg-white">
+          <RadixSelect.Value
+            placeholder={placeholder ? placeholder : 'Select...'}
+            className="max-w-12 overflow-ellipsis capitalize"
+          />
+          <RadixSelect.Icon className="h-5 w-5 -rotate-90">
             <img src="/icons/arrow.svg" alt="arrow" />
-          </RadixSelect.ScrollUpButton>
-          <RadixSelect.Viewport className="bg-white">
-            <RadixSelect.Group>
-              <SelectItem value="apple">Apple</SelectItem>
-              <SelectItem value="banana">Banana</SelectItem>
-              <SelectItem value="blueberry">Blueberry</SelectItem>
-              <SelectItem value="grapes">Grapes</SelectItem>
-              <SelectItem value="pineapple">Pineapple</SelectItem>
-            </RadixSelect.Group>
-          </RadixSelect.Viewport>
-          <RadixSelect.ScrollDownButton className="flex h-5 -rotate-90 cursor-default items-center justify-center bg-white">
-            <img src="/icons/arrow.svg" alt="arrow" />
-          </RadixSelect.ScrollDownButton>
-        </RadixSelect.Content>
-      </RadixSelect.Portal>
-    </RadixSelect.Root>
+          </RadixSelect.Icon>
+        </RadixSelect.Trigger>
+        <RadixSelect.Portal>
+          <RadixSelect.Content
+            className="overflow-hidden rounded-md bg-white shadow-xl"
+            align="center"
+            position="popper"
+            sideOffset={10}
+          >
+            <RadixSelect.ScrollUpButton className="flex h-5 w-5 rotate-90 cursor-default items-center justify-center bg-white">
+              <img src="/icons/arrow.svg" alt="arrow" />
+            </RadixSelect.ScrollUpButton>
+            <RadixSelect.Viewport className="bg-white">
+              <RadixSelect.Group>
+                {data.map((item) => (
+                  <SelectItem key={item} value={item} className="capitalize">
+                    {item}
+                  </SelectItem>
+                ))}
+              </RadixSelect.Group>
+            </RadixSelect.Viewport>
+            <RadixSelect.ScrollDownButton className="flex h-5 -rotate-90 cursor-default items-center justify-center bg-white">
+              <img src="/icons/arrow.svg" alt="arrow" />
+            </RadixSelect.ScrollDownButton>
+          </RadixSelect.Content>
+        </RadixSelect.Portal>
+      </RadixSelect.Root>
+    </div>
   )
 }
 
