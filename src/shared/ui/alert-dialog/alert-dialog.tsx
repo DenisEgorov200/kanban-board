1be4bcf366a9c258db04cfd70cb6225727368531
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-07-01 19:24:40 +0300

    style: add animation

--- src/shared/ui/alert-dialog/alert-dialog.tsx
+++ src/shared/ui/alert-dialog/alert-dialog.tsx
@@ -6,24 +6,24 @@ export const AlertDialog = ({ open, onOpenChange }: AlertDialogProps) => {
     <RadixAlertDialog.Root open={open} onOpenChange={onOpenChange}>
       <RadixAlertDialog.Trigger />
       <RadixAlertDialog.Portal>
-        <RadixAlertDialog.Overlay className="data-[state=open]:animate-overlayShow fixed inset-0" />
-        <RadixAlertDialog.Content className="data-[state=open]:animate-contentShow fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-sm focus:outline-none">
+        <RadixAlertDialog.Overlay className="data-[state=open]:animate-overlayShow fixed inset-0 bg-black/5" />
+        <RadixAlertDialog.Content className="data-[state=open]:animate-contentShow fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-md bg-white p-6 shadow-sm focus:outline-none">
           <RadixAlertDialog.Title className="m-0 text-[17px] font-medium">
             Are you absolutely sure?
           </RadixAlertDialog.Title>
           <RadixAlertDialog.Description className="mb-5 mt-4 text-[15px] leading-normal">
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            This action cannot be undone. This will permanently delete your task
+            and remove your data from our servers.
           </RadixAlertDialog.Description>
           <div className="flex justify-end gap-[25px]">
             <RadixAlertDialog.Cancel asChild>
-              <button className="text-mauve11 bg-mauve4 hover:bg-mauve5 focus:shadow-mauve7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
+              <button className="inline-flex items-center justify-center rounded border border-gray-200 px-4 py-3 font-medium leading-none outline-none transition-colors hover:bg-gray-50 focus:shadow-lg">
                 Cancel
               </button>
             </RadixAlertDialog.Cancel>
             <RadixAlertDialog.Action asChild>
-              <button className="text-red11 bg-red4 hover:bg-red5 focus:shadow-red7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
-                Yes, delete account
+              <button className="inline-flex items-center justify-center rounded bg-red-500 px-4 py-3 font-medium leading-none text-white outline-none transition-colors hover:bg-red-600 focus:shadow-lg">
+                Yes, delete task
               </button>
             </RadixAlertDialog.Action>
           </div>
