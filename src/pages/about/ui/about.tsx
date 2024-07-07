b5ef615bc49bbda4ec7c16938eb2643a6504f9a8
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-07-07 22:55:01 +0300

    style: add info to about-page

--- src/pages/about/ui/about.tsx
+++ src/pages/about/ui/about.tsx
@@ -1,7 +1,16 @@
 export const AboutPage = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="flex h-full flex-col items-center justify-center gap-4">
+      <h1 className="text-center text-4xl font-bold text-gray-500">
+        About the Kanban app
+      </h1>
+      <p className="mb-6 max-w-[450px] text-center text-lg text-gray-400">
+        Kanban is a project management methodology that helps organize work on
+        tasks in the form of columns or boards.
+      </p>
+      <div className="flex h-96 w-96 items-center justify-center rounded-full bg-blue-600">
+        <img src="/gif/duck.gif" alt="duck" />
+      </div>
     </div>
   )
 }
